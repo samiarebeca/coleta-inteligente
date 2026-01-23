119f2b06c9410976fb8a4bfaacd2c97c9df3908d
tocktecnologia <tocktec@gmail.com>
2026-01-23 19:15:20 -0300

    fix white inital page

--- src/App.tsx
+++ src/App.tsx
@@ -17,7 +17,7 @@ const App = () => (
         <TooltipProvider>
           <Toaster />
           <Sonner />
-          <BrowserRouter>
+          <BrowserRouter basename="/coleta-inteligente/">
             <Routes>
               <Route path="/" element={<Index />} />
               <Route path="*" element={<NotFound />} />
--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
+  base: "/coleta-inteligente/",
   server: {
     host: "::",
     port: 8080,
